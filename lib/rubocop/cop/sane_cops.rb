968db6f1c951be7174827fbcd4bcb04c8fa36f8c
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2026-04-06 13:00:11 +0100

    Create DeleteComment cop

--- lib/rubocop/cop/sane_cops.rb
+++ lib/rubocop/cop/sane_cops.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "sane/delete_comment"
 require_relative "sane/disallow_methods"
 require_relative "sane/conditional_assignment_allow_ternary"
 require_relative "sane/empty_line_before_comment"
